a9258a59e7d759bcc092f5caff191dd949ff77f3
william.austin <william.austin@ATTPA-1169>
2019-10-09 13:38:09 -0500

    initial commit

---
+++ xamApp4/xamApp4/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using xamApp4.Services;
+using xamApp4.Views;
+
+namespace xamApp4
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ xamApp4/xamApp4/Views/MapPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace xamApp4.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+       
+        }
+
+        protected override async void OnAppearing()
+        {
+            try
+            {
+                Permission[] perms;
+                perms = new Permission[1];
+                perms[0] = Permission.Location;
+
+                //var status2 = await CrossPermissions.Current.RequestPermissionsAsync(perms);
+
+
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
+                    {
+                        await DisplayAlert("Need location", "Gunna need that location", "OK");
+                    }
+
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Location);
+                    status = results[Permission.Location];
+                }
+
+                if (status == PermissionStatus.Granted)
+                {
+                    ShopsMap.IsShowingUser = true;
+                    //Permission granted, do what you want do.
+                }
+                else //if (status != PermissionStatus.Unknown)
+                {
+                    await DisplayAlert("Location Denied", "Can not continue, try again.", "OK");
+                }
+
+                ShopsMap.MoveToRegion(
+                     MapSpan.FromCenterAndRadius(
+                         new Position(45.172285, -93.874306), Distance.FromMeters(50.00D)));
+
+                //var zoomLevel = 18; // pick a value between 1 and 18
+                //var latlongdeg = 360 / (Math.Pow(2, zoomLevel));
+                //ShopsMap.MoveToRegion(new MapSpan(ShopsMap.VisibleRegion.Center, latlongdeg, latlongdeg));
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
