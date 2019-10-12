27fd42fda5be9146d686e06f130eb6dbaaec52c9
william.austin <william.austin@ATTPA-1169>
2019-10-11 22:11:52 -0500

    start clean up on grid rows/columns.  fix missing msbuild updatedesignxaml on drawable image in project

--- xamApp4/xamApp4/ViewModels/PinItemViewModel.cs
+++ xamApp4/xamApp4/ViewModels/PinItemViewModel.cs
@@ -17,11 +17,14 @@ namespace xamApp4.ViewModels
 
 		public PinItemViewModel()
 		{
+			// TODO: read these from db?  sqlite or firebase?
 			_locations = new ObservableCollection<Location>()
 			{
-				new Location(1,"8 1st Ave S, Buffalo, MN 55313", "Ritzy Replay", new Position(45.170633, -93.874608),"RitzyReplay"),
-				new Location(2,"8 1st Ave S, Buffalo, MN 55313", "Evelyns Winebar", new Position(45.172654, -93.876058),"RitzyReplay"),
-				new Location(3,"8 1st Ave S, Buffalo, MN 55313", "A Wreath Of Franklin", new Position(45.172654, -93.876058),"RitzyReplay")
+				new Location(1,"8 1st Ave S, Buffalo, MN 55313", "Ritzy Replay", new Position(45.171716, -93.874632),"RitzyReplay"),
+				new Location(2,"18 Central Ave, Buffalo, MN 55313", "Evelyns Winebar", new Position(45.172654, -93.876058),"EvelynsWineBar"),
+				new Location(3,"11 Division Arc, Buffalo, MN 55313", "A Wreath Of Franklin", new Position(45.172138, -93.875817),"AWreathOfFranklin"),
+				new Location(4,"112 1st St S, Buffalo, MN 55313", "Hayes Public House", new Position(45.170583, -93.874611),"HayesPublicHouse"),
+	
 			};
 		}
 	}
